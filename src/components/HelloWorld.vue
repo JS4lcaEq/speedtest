b9cdfc16dd3862023111febd0caf92e23b46b879
Condorrr <condorrr@gmail.com>
2021-07-01 01:39:49 +0300

    rc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,96 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    items: 
+    <input v-model="recordsCount" />
+    <button @click="create">CREATE</button>
+    <button @click="sort">SORT (order by value)</button>
+    <button @click="unSort">UNSORT (order by id)</button>
+    
+    {{arr.length}} items in {{length}} ms
+    <p>{{ debug }}</p>
+    <canvas id="canvas"></canvas>
   </div>
 </template>
 
 <script>
+let canvas;
+let context;
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      recordsCount: 50000,
+      arr: [],
+      debug: "",
+      start: 0,
+      end: 0,
+      length: 0
+    };
+  },
+  methods: {
+    render() {
+      let k = 500 / this.arr.length;
+      let i = 0
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      this.arr.forEach((e) => {
+        context.fillRect(i * k, e.dt * k, 1, 1);
+        i++
+      });
+    },
+    create() {
+      this.start = new Date().valueOf()
+      let arr_ = [];
+      let i = 0;
+
+      for (i = 0; i < this.recordsCount; i++) {
+        let dot = { id: i, dt: Math.round(Math.random() * this.recordsCount) };
+        arr_.push(dot);
+      }
+      this.arr = arr_;
+      this.end = new Date().valueOf()
+      this.length = this.end - this.start
+      this.render();
+    },
+    sort() {
+      this.start = new Date().valueOf()
+      this.arr.sort(function (a, b) {
+        return a.dt - b.dt;
+      });
+      this.end = new Date().valueOf()
+      this.length = this.end - this.start
+      this.render();
+    },
+    unSort() {
+      this.start = new Date().valueOf()
+      this.arr.sort(function (a, b) {
+        return a.id - b.id;
+      });
+      this.end = new Date().valueOf()
+      this.length = this.end - this.start
+      this.render();
+    },
+  },
+  mounted() {
+    canvas = document.getElementById("canvas");
+    canvas.width = 500;
+    canvas.height = 500;
+    context = canvas.getContext("2d");
+    this.debug += "w=" + canvas.width + " h=" + canvas.height;
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+#canvas {
+  background-color: #ccc;
+  /* width: 500px; */
+  /* height: 500px; */
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button {
+  margin: 5px;
 }
 </style>
